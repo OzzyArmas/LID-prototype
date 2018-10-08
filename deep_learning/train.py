6e378f1b0674e91b08e66ee4643a14b5a12d4a30
Osvaldo Armas <oosv@amazon.com>
2018-10-08 16:17:59 -0700

    torch.utils.data

--- deep_learning/train.py
+++ deep_learning/train.py
@@ -23,6 +23,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as f
+import torch.utils.data
 
 # important SageMaker predefined paths
 prefix = '/opt/ml/'
