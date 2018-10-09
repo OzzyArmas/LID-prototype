a83d9774d16fab3e6c9e245e4da4503b56056ff9
Osvaldo Armas <oosv@amazon.com>
2018-10-08 17:11:54 -0700

    more test stuff broken

--- deep_learning/train.py
+++ deep_learning/train.py
@@ -22,7 +22,7 @@ import logging
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as f
+import torch.nn.functional as F
 import torch.utils.data
 
 # important SageMaker predefined paths
